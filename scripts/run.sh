455c6c2daff4ea4cd26225dca3180ff0a63f64cb
Rahul Kadyan <hi@znck.me>
2015-10-14 21:14:02 +0530

    Add python.sh to runner array

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-runners=("c++.sh")
+runners=("c++.sh" "python.sh")
 
-for runner in $runners; do
+for runner in "${runners[@]}"; do
   "$root/$runner"
 done
